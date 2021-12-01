8e8234dd70a4d9b6f9c13338dc135f0bd4431da7
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-01 15:44:05 +0100

    add Fixed point class

---
+++ project/FixedPoint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lleguill on 01.12.21.
+//
+
+#include "FixedPoint.h"
---
+++ project/FixedPoint.h
@@ -0,0 +1,14 @@
+//
+// Created by lleguill on 01.12.21.
+//
+
+#ifndef PCSC_PROJECT_FIXEDPOINT_H
+#define PCSC_PROJECT_FIXEDPOINT_H
+
+
+class FixedPoint {
+
+};
+
+
+#endif //PCSC_PROJECT_FIXEDPOINT_H
