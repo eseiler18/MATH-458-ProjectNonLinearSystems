8987c7e3f7c99a98c97f569e27eba3dd7a548cda
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-01 15:13:23 +0100

    add subdirectory

---
+++ project/AbstractSolver.cpp
@@ -0,0 +1,12 @@
+//
+// Created by eseiler@INTRANET.EPFL.CH on 27.11.21.
+//
+
+#include "AbstractSolver.h"
+
+
+AbstractSolver::AbstractSolver()
+        : lowerBound(), upperBound(), initialValue(),
+        tolerance(), maxIter(), f(0), df(0) {}
+
+AbstractSolver::~AbstractSolver() {}
