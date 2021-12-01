9059bb6adc4cfee7c342b4d1bb2557c057d42b6e
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-01 14:59:17 +0100

    add Abstract Solveur class

---
+++ AbstractSolver.h
@@ -0,0 +1,47 @@
+//
+// Created by eseiler@INTRANET.EPFL.CH on 27.11.21.
+//
+
+#ifndef PROJECT_ABSTRACTSOLVER_H
+#define PROJECT_ABSTRACTSOLVER_H
+#include <iostream>
+
+class AbstractSolver {
+public:
+    // Constructor and destructor
+    AbstractSolver();
+    virtual ~AbstractSolver();
+
+    // Other public methods
+    void SetInterval(const double a, const double b) {lowerBound = a; upperBound = b; }
+    void SetInitialValue(const double x0) {initialValue = x0; }
+    void SetTol(const double tol) {tolerance = tol; }
+    void SetMaxIter(const int nmax) {maxIter = nmax; }
+    void SetF(double (*fun)(double x)) {f = fun; }
+    void SetDf(double (*fun)(double x)) {df = fun; }
+
+    // Get methods
+    double GetLowerBound() const { return lowerBound; }
+    double GetUpperBound() const { return upperBound; }
+    double GetInitialValue() const { return initialValue; }
+    double GetTol() const { return tolerance; }
+    int GetMaxIter() const { return maxIter; }
+
+    //Get functions value methods
+    double GetFvalue(double x) const { return f(x); }
+    double GetDfvalue(double x) const { return df(x); }
+
+    // Solver methods (Pure Virtual)
+    virtual void SolveEquation() = 0;
+
+protected:
+    double lowerBound;
+    double upperBound;
+    double initialValue;
+    double tolerance;
+    int maxIter;
+    double (*f)(double x);
+    double (*df)(double x);
+};
+
+#endif //PROJECT_ABSTRACTSOLVER_H
