6bf161e5b34dda5f1a422423c91fc7cdeb098f1b
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-02 21:59:31 +0100

    Some Test for my function Bisection and Fixed point

--- project/main_louis.cpp
+++ project/main_louis.cpp
@@ -2,3 +2,28 @@
 // Created by lleguill on 01.12.21.
 //
 
+#include <iostream>
+#include "Bisection.h"
+#include "FixedPoint.h"
+
+
+double ideFun(double x){return x-22;}
+double dIdeFun(double x){return 1;}
+
+int main() {
+    Bisection b;
+    b.SetF(ideFun);
+    b.SetMaxIter(100);
+    b.SetTol(0.0001);
+    b.SolveEquation();
+
+    /*
+    FixedPoint fp;
+    fp.SetF(ideFun);
+    fp.SetInitialValue(-5);
+    fp.SetMaxIter(100);
+    fp.SetTol(0.0001);
+    fp.SolveEquation(); */
+
+    return 0;
+}
