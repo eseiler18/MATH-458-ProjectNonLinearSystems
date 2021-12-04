3134dabb3f0b414e82b12b77ea39b0d6df427f0d
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-04 16:05:44 +0100

    add class abstract token to str -> function

---
+++ project/parser/AbstractToken.h
@@ -0,0 +1,41 @@
+//
+// Created by eseiler@INTRANET.EPFL.CH on 03.12.21.
+//
+
+#ifndef PCSC_PROJECT_ABSTRACTTOKEN_H
+#define PCSC_PROJECT_ABSTRACTTOKEN_H
+
+#include <iostream>
+#include "TokenType.h"
+
+/**
+ * Token common class representing a syntacic element of the operation
+ */
+class AbstractToken{
+protected:
+    AbstractToken(TokenType t):tokenType(t){}
+    TokenType tokenType;
+
+public:
+    virtual ~AbstractToken()= default;
+    // Methods
+    bool isUnary() const{
+        if (tokenType == TokenType::NUMBER || tokenType == TokenType::VAR){ return true;}
+        else{ return false;}
+    }
+
+    TokenType getTokenType() const {
+        return tokenType;
+    }
+
+    bool isOperator() const{
+        if (tokenType == TokenType::ADD || tokenType == TokenType::SUB || tokenType == TokenType::MUL ||
+        tokenType == TokenType::DIV || tokenType==TokenType::POW) {return true;}
+        else{ return false;}
+    }
+
+    virtual std::string toString() const = 0;
+
+
+};
+#endif //PCSC_PROJECT_ABSTRACTTOKEN_H
