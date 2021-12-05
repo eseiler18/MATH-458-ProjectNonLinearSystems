ce77c341ab69898d84a6f5ae314a18d77e7954e8
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-05 23:57:09 +0100

    add builder class (build the function from tokens)

---
+++ project/parser/Builder.cpp
@@ -0,0 +1,70 @@
+//
+// Created by eseiler@INTRANET.EPFL.CH on 04.12.21.
+//
+
+#include "Builder.h"
+
+OperatorNode * Builder::buildOperator(const std::list<AbstractNode *>& operands, AbstractToken *_operator) {
+    if(operands.size() !=2 ){
+        throw std::invalid_argument("Illegal operands combination with " + _operator->toString());
+    }
+    return new OperatorNode(_operator->getTokenType(), operands.front(), operands.back());
+}
+
+AbstractNode *Builder::buildUnitary(const AbstractToken *unitaryToken){
+    if(unitaryToken->getTokenType() == TokenType::NUMBER){
+        const std::string& numberStr = ((Token*)unitaryToken)->getValueStr();
+        double number = std::stod(numberStr);
+        return new NumberNode(number);
+    }
+    if(unitaryToken->getTokenType() == TokenType::VAR){
+        return new VariableNode();
+    }
+    if(unitaryToken->getTokenType() == TokenType::CONTAINER) {
+        return buildTokens(unitaryToken->getChildren());
+    }
+    else{
+        throw std::invalid_argument("Invalid token type for buildUnitary");
+    }
+}
+
+AbstractNode *Builder::buildTokens(std::list<AbstractToken *> tokens) {
+    if(tokens.empty()){return nullptr; }
+    if(tokens.size() == 1){
+        return buildUnitary(tokens.front());
+    }
+    std::list <AbstractNode*> operands;
+    AbstractToken* _operator = nullptr;
+    for(AbstractToken* currentToken : tokens){
+        if(currentToken->isOperator()) {
+            if (_operator == nullptr) {
+                _operator = currentToken;
+            } else {
+                OperatorNode *subExpression = buildOperator(operands, _operator);
+                operands.clear();
+                operands.push_back(subExpression);
+                _operator = currentToken;
+            }
+        }
+        else{
+            operands.push_back(buildToken(currentToken));
+        }
+    }
+    if(_operator != nullptr){
+        return buildOperator(operands, _operator);
+    }
+    return operands.front();
+}
+
+AbstractNode *Builder::buildToken(AbstractToken *token) {
+    if(token->getTokenType() == TokenType::CONTAINER){
+        return buildTokens(token->getChildren());
+    }
+    else{
+        return buildUnitary(dynamic_cast<const Token *>(token));
+    }
+}
+
+AbstractNode * Builder::build() {
+    return buildTokens(tokensOfEquation.getChildren());
+}
---
+++ project/parser/Builder.h
@@ -0,0 +1,30 @@
+//
+// Created by eseiler@INTRANET.EPFL.CH on 04.12.21.
+//
+
+#ifndef PCSC_PROJECT_BUILDER_H
+#define PCSC_PROJECT_BUILDER_H
+
+#include <list>
+#include "TokenContainer.h"
+#include "NumberNode.h"
+#include "VariableNode.h"
+#include "OperatorNode.h"
+#include "TokenType.h"
+
+class Builder {
+public:
+    Builder(const TokenContainer& tokenContainer):tokensOfEquation(tokenContainer){}
+    AbstractNode * build();
+
+
+private:
+    TokenContainer tokensOfEquation;
+    static OperatorNode* buildOperator(const std::list <AbstractNode*>& operands, AbstractToken* _operator);
+    static AbstractNode* buildUnitary(const AbstractToken* unitaryNode);
+    static AbstractNode* buildTokens(std::list <AbstractToken*> tokens);
+    static AbstractNode* buildToken(AbstractToken* token);
+};
+
+
+#endif //PCSC_PROJECT_BUILDER_H
