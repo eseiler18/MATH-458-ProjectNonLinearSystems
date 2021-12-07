115826572e9a3a96140cf39f66ec5ffa438a076e
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-07 11:28:51 +0100

    Correction of the AbstractSolver Class constructor

--- project/AbstractSolver.cpp
+++ project/AbstractSolver.cpp
@@ -6,7 +6,15 @@
 
 
 AbstractSolver::AbstractSolver()
-        : lowerBound(), upperBound(), initialValue(),
-        tolerance(), maxIter(), f(0), df(0) {}
+        :tolerance(), maxIter(), f(0) {}
+
+AbstractSolver::AbstractSolver(double (*fun)(double))  {
+    f =fun ;
+    tolerance=0.00001;
+    maxIter= 10000 ;
+}
+
+AbstractSolver::AbstractSolver(double (*fun)(double), double tol, int Maxit) :
+    f(fun),tolerance(tol),maxIter(Maxit) {}
 
 AbstractSolver::~AbstractSolver() = default;
