9d19b82a33cb19fb26db184d8c6e2e60a409ae8c
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-09 15:04:32 +0100

    test try catch

--- project/main.cpp
+++ project/main.cpp
@@ -6,80 +6,19 @@
 #include "Newton.h"
 #include "ClassicChord.h"
 
-double fun(double x){return x*x;}
-double dFun(double x){return 2*x;}
-double ideFun(double x){return x-22;}
-double dIdeFun(double x){return 1;}
-
-void readCsvLine() {
-    // File pointer
-    std::ifstream fin;
-
-    // Open an existing file
-    fin.open("data.csv", std::ios::in);
-    assert(fin.is_open());
-
-    // Get the function number of which the data is required
-    int i, count = 0;
-    std::cout << "Enter the row number of the file you want to read : ";
-    std::cin >> i;
-
-    // Read the Data from the file as String Vector
-    std::vector<std::string> row;
-    std::string line, word;
-
-    while (!fin.eof()) {
-        row.clear();
-        // read an entire row and store it in a string variable 'line'
-        getline(fin, line);
-        // used for breaking words
-        std::stringstream s(line);
-        // read every column data of a row and store it in a string variable, 'word'
-        while (getline(s, word, ',')) {
-            // add all the column data of a row to a vector
-            row.push_back(word);
-        }
-        // Compare the roll number
-        if (count == i) {
-            // Print the found data
-            count = 1;
-            std::cout << "Function: " << row[0] << "\n";
-            std::cout << "Derivative: " << row[1] << "\n";
-            std::cout << "Interval: [" << row[2] << ", " << row[3] << "]" "\n";
-            std::cout << "Initial Value: " << row[4] << "\n";
-            std::cout << "Tolerance: " << row[5] << "\n";
-            std::cout << "Max Iterations: " << row[6] << "\n";
-            break;
-        }
-        count+=1;
-    }
-}
-
 int main() {
 
-
-    Newton k(ideFun,dIdeFun,6);
-    k.SolveEquation();
-
-
-    Newton n(fun,dFun,20,0.00001,1000);
-    n.SolveEquation();
-
-    ClassicChord c(fun,0);
-
-
-    /*
-
-    ClassicChord ch;
-    ch.SetF(ideFun);
-    ch.SetInitialValue(-5);
-    ch.SetMaxIter(100);
-    ch.SetTol(0.0001);
-    ch.SolveEquation();
-
-    readCsvLine(); */
-
-
-
+    std::cout << "Main Emilien" << std::endl;
+    for (int i=0; i<3; i++) {
+        try {
+            if (i == 1) {
+                throw std::invalid_argument("ahaha");
+            } else {
+                std::cout << i << std::endl;
+            }
+        } catch (std::invalid_argument &e) {
+            std::cout <<  e.what() << std::endl;
+        }
+    }
     return 0;
 }
