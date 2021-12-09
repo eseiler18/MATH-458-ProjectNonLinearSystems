3381c7df395fc152900aacdf7bb5ab457d607a09
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-09 21:55:51 +0100

    add Doxygen comment

--- project/parser/AbstractToken.h
+++ project/parser/AbstractToken.h
@@ -5,12 +5,13 @@
 #ifndef PCSC_PROJECT_ABSTRACTTOKEN_H
 #define PCSC_PROJECT_ABSTRACTTOKEN_H
 
-#include <iostream>
+#include <string>
 #include <list>
 #include "TokenType.h"
 
 /**
- * Token common class representing a syntacic element of the operation
+ * Token common class representing a syntactic element of the operation
+ * A token has a token type in parameter (see TokenType.h list all token type
  */
 class AbstractToken{
 protected:
@@ -19,25 +20,24 @@ protected:
 
 public:
     virtual ~AbstractToken()= default;
-    // Methods
+    /// getter of the token type
+    TokenType getTokenType() const {
+        return tokenType;
+    }
+    /// method to know if token is unary (number or variable)
     bool isUnary() const{
         if (tokenType == TokenType::NUMBER || tokenType == TokenType::VAR){ return true;}
         else{ return false;}
     }
-
-    TokenType getTokenType() const {
-        return tokenType;
-    }
-
+    /// method to know if token is an operator
     bool isOperator() const{
         if (tokenType == TokenType::ADD || tokenType == TokenType::SUB || tokenType == TokenType::MUL ||
         tokenType == TokenType::DIV || tokenType==TokenType::POW) {return true;}
         else{ return false;}
     }
-
+    /// to string method to visualize and help debug
     virtual std::string toString() const = 0;
-    virtual const std::list<AbstractToken *> &getChildren() const{}
-
-
+    /// pure virtual method to get children of TokenContainer
+    virtual const std::list<AbstractToken *> &getChildren() {}
 };
 #endif //PCSC_PROJECT_ABSTRACTTOKEN_H
