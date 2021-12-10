85caad32454ef2bb690abb1201d8102b98d16793
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-10 11:52:04 +0100

    Update Catch/trow error on the Method Bisection

--- project/Bisection.cpp
+++ project/Bisection.cpp
@@ -22,61 +22,79 @@ Bisection::Bisection(AbstractNode* fun,double tol, int Maxit, double lowerbound,
 Bisection::~Bisection() =default;
 
 void Bisection::SolveEquation() const {
-    double b=upperBound;
-    double a=lowerBound;
-    std::cout<<"Bisection Method :"<<std::endl;
+    try {
+        double b = upperBound;
+        double a = lowerBound;
+        std::cout << "Bisection Method :" << std::endl;
 
-    //Tcheck 1 : Bound viability
-    //assert(b>a);
+        //check 1 : Bound viability
+        if (b < a) {
+            std::cout << "Higer bound is smaller than lower bound, bounds inverted for the calculation. " << std::endl;
+            double aux = a;
+            a = b;
+            b = aux;
+        }
 
-    //Tcheck 2 : Solution possible ?
-    //assert(GetFValue(a)* GetFValue(b)<0);
+        //check 2 : Solution possible ?
+        //assert(GetFValue(a)* GetFValue(b)<0);
+        if (GetFValue(a) * GetFValue(b) > 0) {
+            throw std::invalid_argument("f(a)*f(b) >0 , can't do the Bisection method");
+        }
 
-    //Tcheck Direct Solution
-    if (GetFValue(a)==0){
-        std::cout<<"A Solution is the lowerBound : x= "<<a<<std::endl;
-        return;
-    }
-    if (GetFValue(b)==0){
-        std::cout<<"A Solution is the Upperbound : x= "<<b<<std::endl;
-        return;
-    }
+        //Tcheck Direct Solution
+        if (GetFValue(a) == 0) {
+            std::cout << "A Solution is the lowerBound : x= " << a << std::endl;
+            return;
+        }
+        if (GetFValue(b) == 0) {
+            std::cout << "A Solution is the Upperbound : x= " << b << std::endl;
+            return;
+        }
 
-    //Initialise
-    double aux = (a+b)/2;
-    double res = (b-a)/2;
-    int it = 1;
+        //Initialise
+        double aux = (a + b) / 2;
+        double res = (b - a) / 2;
+        int it = 1;
 
-    while (res > tolerance && it < maxIter) {
-        if (GetFValue(aux)==0){
-            std::cout << "Exact solution founded after  " << it << " iterations "<< std::endl;
+        while (res > tolerance && it < maxIter) {
+            if (GetFValue(aux) == 0) {
+                std::cout << "Exact solution founded after  " << it << " iterations " << std::endl;
+                std::cout << "x = " << aux << " and f(x) = " << GetFValue(aux) << std::endl;
+                return;
+            }
+
+            if (GetFValue(a) * GetFValue(aux) < 0) { b = aux; }
+            else if (GetFValue(b) * GetFValue(aux) < 0) { a = aux; }
+            else {
+                std::cout << "Bisection Algorithm not operating correctly" << std::endl;
+                return;
+            }
+            aux = (a + b) / 2;
+            res /= 2;
+            it += 1;
+        }
+
+        if (it >= maxIter) {
+            std::string message("Didn't converge after " + std::to_string(it) + " iterations for a tolerance of "
+                                + std::to_string(tolerance) + ".\nDifference of successive iterates = " +
+                                std::to_string(res) + "x = "
+                                + std::to_string(aux) + " and f(x) = " + std::to_string(GetFValue(aux)));
+            throw ExceptionIterate(message);
+        } else if (std::abs(GetFValue(aux)) > tolerance * 10) {
+            std::string message("Converge to a wrong solution after " + std::to_string(it) +
+                                " iterations for a tolerance of " + std::to_string(tolerance) +
+                                ".\nDifference of successive iterates = "
+                                + std::to_string(res) + "x = " + std::to_string(aux) + " and f(x) = " +
+                                std::to_string(GetFValue(aux)));
+            throw ExceptionIterate(message);
+        } else {
+            std::cout << "Converge after " << it << " iterations for a tolerance of " << tolerance << std::endl;
             std::cout << "x = " << aux << " and f(x) = " << GetFValue(aux) << std::endl;
-            return;
         }
 
-        if (GetFValue(a)* GetFValue(aux)<0){b=aux;}
-        else if (GetFValue(b)* GetFValue(aux)<0){a=aux;}
-        else {
-            std::cout<<"Bisection Algorithm not operating correctly"<<std::endl;
-            return;
-        }
-        aux= (a+b)/2;
-        res /= 2;
-        it +=1;
     }
-
-    if (it >= maxIter){
-        std::cout << "Didn't converge after " << maxIter << " iterations for a tolerance of " << tolerance << std::endl;
-        std::cout << "Difference of successive iterates = " << res;
-        std::cout << "x = " << aux << " and f(x) = " << GetFValue(aux) << std::endl;
-    }
-    else{
-        std::cout << "Converge after " << it << " iterations for a tolerance of " << tolerance << std::endl;
-        std::cout << "x = " << aux << " and f(x) = " << GetFValue(aux) << std::endl;
-    }
-
-
-
+    catch(std::invalid_argument &e){std::cout<<e.what()<<std::endl;}
+    catch (ExceptionIterate(&e)){ e.what(); }
 }
 
 
