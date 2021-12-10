8f61983e552b42bc3c8bf966389401224106263c
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-10 23:00:34 +0100

    Doxygen and comment for the abstractSolver

--- project/AbstractSolver.cpp
+++ project/AbstractSolver.cpp
@@ -4,11 +4,13 @@
 
 #include "AbstractSolver.h"
 
+// Default empty constructor
 AbstractSolver::AbstractSolver()
         :tolerance(), maxIter(), f(0) {}
 
-
+// Classic Constructor that affect the given value to the variables
 AbstractSolver::AbstractSolver(AbstractNode* fun, double tol, int Maxit) :
     f(fun),tolerance(tol),maxIter(Maxit){}
 
+// Default destructor
 AbstractSolver::~AbstractSolver() = default;
--- project/AbstractSolver.h
+++ project/AbstractSolver.h
@@ -10,36 +10,52 @@
 #include "ExceptionIterate.h"
 #include <math.h>
 
+/**
+ * AbstractSolver Class is the mother class of all the numerical method. It contains the variables
+ * common to all method :
+ * - The function
+ * - The max iteration
+ * - The tolerance
+ */
+
 class AbstractSolver {
 public:
     // Constructor and destructor
     AbstractSolver();
+    /// Constructor with the three variable common to all method
     AbstractSolver(AbstractNode* fun ,double tol,int Maxit);
 
     virtual ~AbstractSolver();
 
-
-    // Setter
+    /// Method to set the tolerance
     void SetTol(const double tol) {tolerance = tol; }
+    /// Method to set the max iteration
     void SetMaxIter(const int nMax) {maxIter = nMax; }
+    /// Method to set the function
     void SetF(AbstractNode* fun) {f = fun; }
 
 
     // Getter
+    /// Method to get the tolerance
     double GetTol() const { return tolerance; }
+    /// Method to get the max iteration
     int GetMaxIter() const { return maxIter; }
 
     //Get functions value methods
+    /// Method that return the value of f(x) for a given x
     double GetFValue(double x) const { return f->solve(x); }
 
 
-    // Solver methods (Pure Virtual)
+    /// Pure virtual method that solve f(x)
     virtual void SolveEquation() const = 0;
 
 protected:
+    // function pointer
+    AbstractNode* f ;
+    // Other variables
     double tolerance ;
     int maxIter ;
-    AbstractNode* f ;
+
 };
 
 #endif //PROJECT_ABSTRACTSOLVER_H
