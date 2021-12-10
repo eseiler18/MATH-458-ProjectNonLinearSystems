cd2b0a78cad1d5c0e06426b2da8b7406bbafd526
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-11 00:16:22 +0100

    add comment and Doxygen comment

--- project/ClassicChord.cpp
+++ project/ClassicChord.cpp
@@ -4,27 +4,32 @@
 
 #include "ClassicChord.h"
 
-//Constructor
+// Default empty constructor
 ClassicChord::ClassicChord() = default;
-ClassicChord::ClassicChord(AbstractNode* fun, double intialval, double tol, int Maxit) :
-    AbstractSolver(fun,tol,Maxit), initialValue(intialval){}
 
+// The constructor with data call the constructor with all separated input.
+// It also verify the comptability with the classic chord method
 ClassicChord::ClassicChord(Data *data) {
     if (data->method["Chord"]) {
         *this =ClassicChord(data->fun,data->initialValue,
-                          data->tolerance,data->maxIter);
+                            data->tolerance,data->maxIter);
     }else{
         throw std::invalid_argument("Dont fit with Classic Chord Method");
     }
 }
+// Classic constructor
+ClassicChord::ClassicChord(AbstractNode* fun, double intialval, double tol, int Maxit) :
+    AbstractSolver(fun,tol,Maxit), initialValue(intialval){}
 
-//Destructor
+// Default destructor
 ClassicChord::~ClassicChord() = default;
 
-//Equation Solver
+
+// Solving method
 void ClassicChord::SolveEquation() const {
     try {
         std::cout << "Classic Chord Method with Aikten acceleration:" << std::endl;
+        //Initialise
         int it = 1;
         double xPrev = initialValue;
         double m = 0.1;
@@ -33,37 +38,57 @@ void ClassicChord::SolveEquation() const {
         double Ax;
         double res = std::abs(x - xPrev);
 
+        // Don't stop until we reach the desired tolerance or the max iteration
         while (res > tolerance && it < maxIter) {
+            // m update for iteration n
             m = (x - xPrev) / (GetFValue(x) - GetFValue(xPrev));
+            // chord update for x(n)
             xNext = x - m * GetFValue(x);
+
+            // Update value for the n+1 iteration ( for Aitken )
             xPrev = x;
             x = xNext;
+
+            // m update for iteration n+1
             m = (x - xPrev) / (GetFValue(x) - GetFValue(xPrev));
+            // chord update for x(n+1)
             xNext = x - m * GetFValue(x);
+            // Aitken update
             Ax = xNext - pow(xNext - x, 2) / (xNext + xPrev - 2 * x);
+
+            // update residual : the difference between two successive iterate
             res = std::abs(Ax - xNext);
+
+            // Update value & iterator
             it += 1;
             xPrev = xNext;
             x = Ax;
         }
+
+        // Trowing error for not converging after the given max iteration
         if (it >= maxIter) {
             std::string message("Didn't converge after " + std::to_string(it) + " iterations for a tolerance of "
                                 + std::to_string(tolerance) + ".\nDifference of successive iterates = " +
                                 std::to_string(res) + "x = "
                                 + std::to_string(Ax) + " and f(x) = " + std::to_string(GetFValue(Ax)));
             throw ExceptionIterate(message);
-        } else if (std::abs(GetFValue(Ax)) > tolerance * 10) {
+        }
+        // Throwing error for converging to a wrong solution
+        else if (std::abs(GetFValue(Ax)) > tolerance * 10) {
             std::string message("Converge to a wrong solution after " + std::to_string(it) +
                                 " iterations for a tolerance of " + std::to_string(tolerance) +
                                 ".\nDifference of successive iterates = "
                                 + std::to_string(res) + " x = " + std::to_string(Ax) + " and f(x) = " +
                                 std::to_string(GetFValue(Ax)));
             throw ExceptionIterate(message);
-        } else {
+        }
+        // else : printing the converged solution
+        else {
             std::cout << "Converge after " << it << " iterations for a tolerance of " << tolerance << std::endl;
             std::cout << "x = " << Ax << " and f(x) = " << GetFValue(Ax) << std::endl;
         }
     }
+    // Catch for the throw error
     catch (ExceptionIterate(&e)){ e.what(); }
     catch (std::invalid_argument(&e)) { std::cout << e.what() << std::endl; }
 }
--- project/ClassicChord.h
+++ project/ClassicChord.h
@@ -6,26 +6,33 @@
 #define PROJECT_CLASSICCHORD_H
 #include "AbstractSolver.h"
 
-class ClassicChord : public AbstractSolver{
-    public:
 
-    // Generator and Destructor
+/**
+ * Daughter of AbstractSolver, that solve the Equation with Classic Chord Method.
+ * Classic chord Method need to work with a initial value.
+ */
+
+class ClassicChord : public AbstractSolver{
+public:
+    /// Empty constructor
     ClassicChord();
-    ClassicChord(AbstractNode* fun,double intialvalue,double tol,int Maxit);
+    /// Constructor with a data vector
     ClassicChord(Data* data);
+    /// Constructor with variable spared
+    ClassicChord(AbstractNode* fun,double intialvalue,double tol,int Maxit);
+    /// Destructor
     ~ClassicChord() override;
 
-    // Getter
+    /// Getter method  for the initial value.
     double GetInitialValue() const { return initialValue; }
 
-    //Setter
+    /// Setter method  for the initial value.
     void SetInitialValue(const double x0) {initialValue = x0; }
 
-    //Solve method
+    /// Polymorph solver for classic chord method
     void SolveEquation() const override;
 
 private:
     double initialValue;
 };
-
 #endif //PROJECT_CLASSICCHORD_H
