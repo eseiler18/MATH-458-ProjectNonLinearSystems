123a8d7d5a2d8c3d1af8ce6bdb6b1b2eb3bdcccf
eseiler@INTRANET.EPFL.CH <emilien.seiler@epfl.ch>
2021-12-14 14:24:42 +0100

    add Newton methods for system method

--- project/main.cpp
+++ project/main.cpp
@@ -1,40 +1,43 @@
-#include <cstdlib>      // EXIT_FAILURE, etc
-#include <string>
 #include <iostream>
-#include <fstream>
-#include <dlfcn.h>      // dynamic library loading, dlopen() etc
-#include <memory>
-#include <unistd.h>
-#include <chrono>
-#include <thread>
+#include "solverMethods/NewtonSystem.h"
+#include "readData/InterpreterInputFunction.h"
+#include "readData/parser/Solver.h"
+#include "solverMethods/ExceptionIterate.h"
 
-
-
-using myP = double(*)(double);
 int main(int argc , char* argv[]) {
-    std::string cppfile="../../functions.cpp";
-    const char *libfile="../../malib.so";
-    // le résultat de la compile est dans log file ca revient à faire:
-    // g++ - Wall -Wextra monfichier.cpp -o malib.so
-    std::string logfile="../../runtimecode.log";
-    std::string cmd = "g++ -Wall -Wextra " + cppfile + " -o " + libfile + " -O2 -shared -fPIC &> " + logfile;
-    remove(libfile);
-    system(cmd.c_str());
-    std::this_thread::sleep_for(std::chrono::milliseconds(400) );
-    void * lib = nullptr;
-    while (lib == nullptr){
-        lib =dlopen(libfile, RTLD_LAZY);
-    }
-    if(lib) {
-        myP  fn = (myP)(dlsym(lib, "fun"));
-        if(fn) {
-            std::cout << fn(1) << std::endl;
-
+    try {
+        int nbVariable = 2;
+        double x[2] = {2.0, 3.0};
+        AbstractNode *f1 = Solver::strToFun("x0^2+x0*x1-10");
+        AbstractNode *f2 = Solver::strToFun("x1+3*x0*x1^2-57");
+        Eigen::VectorX<AbstractNode *> F(nbVariable);
+        F(0) = f1;
+        F(1) = f2;
+        AbstractNode *f1dx = Solver::strToFun("2x0+x1");
+        AbstractNode *f2dx = Solver::strToFun("3*x1^2");
+        AbstractNode *f1dy = Solver::strToFun("x0");
+        AbstractNode *f2dy = Solver::strToFun("1+3*x0*x1");
+        Eigen::MatrixX<AbstractNode *> J(nbVariable, nbVariable);
+        J(0, 0) = f1dx;
+        J(0, 1) = f1dy;
+        J(1, 0) = f2dx;
+        J(1, 1) = f2dy;
+        double x0[2] = {10, 5};
+        NewtonSystem sys(nbVariable, F, J, 0.00000001, 10000, x0);
+        double *xRes = sys.solveSystem();
+        std::string resultStr;
+        std::string FResultStr;
+        for (int i=0 ; i<nbVariable;i++){
+            resultStr.append(std::to_string(xRes[i]));
+            FResultStr.append(std::to_string(sys.getFValue(xRes)(i)));
+            resultStr.append(",");
+            FResultStr.append(",");
         }
-        dlclose(lib);
+        std::cout << "Converge after " << sys.getMaxIter() << " iterations for a tolerance of " <<
+        sys.getTolerance() << std::endl;
+        std::cout << "x = " << resultStr << " and f(x) = " << FResultStr << std::endl;
     }
-    else{
-        throw std::invalid_argument("lib null");
-    }
-
+    catch(ExceptionIterate &e) {e.what();}
+    catch (ParserException &e) {std::cout << e.what() << std::endl;}
+    catch (std::invalid_argument &e) {std::cout << e.what() << std::endl;}
 }
