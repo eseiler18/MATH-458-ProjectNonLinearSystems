fa9e98fdf92a4fc6c908b84201803f2ce162235f
Louis Ernest Le Guillouzic <louis.leguillouzic@epfl.ch>
2021-12-14 23:26:22 +0100

    Move The consturction of the data in the Try to catch eventual exception error ;)

--- project/main_louis.cpp
+++ project/main_louis.cpp
@@ -25,17 +25,18 @@ int main() {
         }
         if (data->method["Newton"]){
             std::cout << "\nNewton Method with Aitken acceleration :" << std::endl;
-            Newton newt(data);
             try {
+                Newton newt(data);
                 aux = newt.SolveEquation();
                 std::cout << "Converge after " << aux.it << " iterations for a tolerance of " << newt.GetTol() << std::endl;
                 std::cout << "x = " << aux.result << " and f(x) = " << newt.GetFValue(aux.result) << std::endl;
             }catch (std::invalid_argument(&e)) { std::cout << e.what() << std::endl; }
             catch (ExceptionIterate(&e)){ std::cout << e.what() << std::endl;}
+
         }if (data->method["Bisection"]){
             std::cout << "\nBisection Method :" << std::endl;
-            Bisection bisec(data);
             try {
+                Bisection bisec(data);
                 aux = bisec.SolveEquation();
                 std::cout << "Converge after " << aux.it << " iterations for a tolerance of " << bisec.GetTol() << std::endl;
                 std::cout << "x = " << aux.result << " and f(x) = " << bisec.GetFValue(aux.result) << std::endl;
@@ -44,17 +45,18 @@ int main() {
 
         }if (data->method["Chord"]) {
             std::cout << "\nClassic Chord Method with Aikten acceleration:" << std::endl;
-            ClassicChord chrd(data);
             try {
+                ClassicChord chrd(data);
                 aux = chrd.SolveEquation();
                 std::cout << "Converge after " << aux.it << " iterations for a tolerance of " << chrd.GetTol() << std::endl;
                 std::cout << "x = " << aux.result << " and f(x) = " << chrd.GetFValue(aux.result) << std::endl;
             }catch (std::invalid_argument(&e)) { std::cout << e.what() << std::endl; }
             catch (ExceptionIterate(&e)){ std::cout << e.what() << std::endl; }
+
         }if (data->method["FixedPoint"]){
             std::cout << "\nFixed Point Method with Aitken acceleration :" << std::endl;
-            FixedPoint fxdp(data);
             try {
+                FixedPoint fxdp(data);
                 aux = fxdp.SolveEquation();
                 std::cout << "Converge after " << aux.it << " iterations for a tolerance of " << fxdp.GetTol() << std::endl;
                 std::cout << "x = " << aux.result << " and f(x) = " << fxdp.GetFValue(aux.result) << std::endl;
