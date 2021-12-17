a2bc971385a4822f5df472124d5c0493e4381f0b
LeGuiz <91272933+LeGuiz@users.noreply.github.com>
2021-12-17 13:08:34 +0100

    Update runSystem.cpp

--- project/runSystem.cpp
+++ project/runSystem.cpp
@@ -5,13 +5,19 @@
 
 int main(int argc , char* argv[]) {
     try {
+        // Two variable sytem
         int nbVariable = 2;
         double x[2] = {2.0, 3.0};
+        
+        // Construc the Abstract node from the matematical expression
         AbstractNode *f1 = Solver::strToFun("x0^2+x0*x1-10");
         AbstractNode *f2 = Solver::strToFun("x1+3*x0*x1^2-57");
+        
         Eigen::VectorX<AbstractNode *> F(nbVariable);
         F(0) = f1;
         F(1) = f2;
+        
+        // Construct the correspondnat Jacobian
         AbstractNode *f1dx = Solver::strToFun("2x0+x1");
         AbstractNode *f2dx = Solver::strToFun("3*x1^2");
         AbstractNode *f1dy = Solver::strToFun("x0");
@@ -21,9 +27,15 @@ int main(int argc , char* argv[]) {
         J(0, 1) = f1dy;
         J(1, 0) = f2dx;
         J(1, 1) = f2dy;
+        
         double x0[2] = {10, 5};
+        
+        // construct the Solver
         NewtonSystem sys(nbVariable, F, J, 0.00000001, 10000, x0);
+        //Solve the System
         double *xRes = sys.solveSystem();
+        
+        //Get the result for each variables
         std::string resultStr;
         std::string FResultStr;
         for (int i=0 ; i<nbVariable;i++){
@@ -32,11 +44,16 @@ int main(int argc , char* argv[]) {
             resultStr.append(",");
             FResultStr.append(",");
         }
+        // Print result
         std::cout << "Converge after " << sys.getMaxIter() << " iterations for a tolerance of " <<
         sys.getTolerance() << std::endl;
         std::cout << "x = " << resultStr << " and f(x) = " << FResultStr << std::endl;
     }
+    
+    // Catch wrong answer exeption
     catch(ExceptionIterate &e) {std::cout << e.what() << std::endl;}
+    //Catch error in the construction of the function
     catch (ParserException &e) {std::cout << e.what() << std::endl;}
+    // Catch wrong argument
     catch (std::invalid_argument &e) {std::cout << e.what() << std::endl;}
 }
